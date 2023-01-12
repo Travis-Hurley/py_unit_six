1ae6d449bfa839d46013e2a6c96d0c2c46892639
Travis-Hurley <hurlean@ssfs.org>
2023-01-12 11:54:40 -0500

    yum

--- assignment_six.py
+++ assignment_six.py
@@ -1,20 +1,18 @@
-#Travis Hurley - Jan - -- - 2023 - Running the birthday paradox
+# Travis Hurley - Jan - -- - 2023 - Running the birthday paradox
 import random
 def get_birthdays():
     list_one = []
-    n = 2
+    n = 2 # change to 23
     for x in range(n):
-        list_one.append(random.randint(1, 3))
+        list_one.append(random.randint(1, 3)) # change to 365
     return list_one
 def is_duplicates(list_one):
-    print()
+    
 def main():
     print("Hello! I will run the birthday function for you as many times as you would like!")
-    while True:
-        amount=input("How many would that be?")
-        if amount == int:
-            break
+    amount = int(input("How many would that be?"))
     for y in range(amount):
         get_birthdays()
+        is_duplicates(list_one)
 main()
 
