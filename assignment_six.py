1c40b5e0aa32d72514a97d1aff0d9296699d70db
Travis-Hurley <hurlean@ssfs.org>
2023-01-18 09:01:07 -0500

    int stuff

--- assignment_six.py
+++ assignment_six.py
@@ -1,31 +1,35 @@
 # Travis Hurley - Jan - -- - 2023 - Running the birthday paradox
 import random
 global amount
-amount=0
+amount = 0
 def get_birthdays():
     list_one = []
-    n = 2 # change to 23
+    n = 23 # change to 23
     for x in range(n):
-        list_one.append(random.randint(1, 3)) # change to 365
-        print(list_one,"list")
+        list_one.append(random.randint(1, 365)) # change to 365
+    print(list_one)
     return list_one
 def is_duplicates(list_one):
-    while True:
-        num = len(list_one)
-        x = list_one[0]
-        list_one[0:(num - 1)] = list_one[1:num]
-        list_one[-1] = x
-        list_one.pop[x]
-        duplicate = list_one.count(x)
-        if duplicate == 1:
-            amount += 1
-            print(amount,"amount")
-            break
-
-
+    print(list_one)
+    v = 0
+    for x in range(0, 23):
+        v += 1
+        var1 = list_one[x]
+        for w in range(v, 23):
+            var2 = list_one[w]
+            if var2 == var1:
+                amount += 1
+                print(amount)
+                return True
 def main():
-    print("Hello! I will run the birthday function for you as many times as you would like!")
-    list_one = int(input("How many would that be?"))
+    while True:
+        try:
+            list_one = int(input("How many times would you like to run the simulation? "))
+        except ValueError:
+            print("Sorry, I didn't understand your entry. Please enter a positive integer.")
+            continue
+        else:
+            break
     for y in range(list_one):
         get_birthdays()
         is_duplicates(list_one)
